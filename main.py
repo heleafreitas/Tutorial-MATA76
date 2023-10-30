07c20714ce4d62b0a93f44c5e6c54cf7a249096b
Helen de Freitas <82972511+heleafreitas@users.noreply.github.com>
2023-10-30 16:18:30 -0300

    Listando tarefas

--- main.py
+++ main.py
@@ -4,6 +4,39 @@ import bd_tutorial as db
 app = Flask(__name__)
 
 @app.route('/')
+def home():
+    lista = listarTarefas()
+    return render_template('home.html', data=lista)
+
+@app.route('/iniciarTarefa', methods=['POST'])
+def iniciarTarefa():
+    id = request.form['id']
+    db.execute_query(f"UPDATE activities SET status='iniciada' WHERE id = {id}")
+    return render_template('home.html', data=id)
+
+def listarTarefas():
+    resposta = db.select_from_table(table_name="activities")
+    lista = []
+    for i in range(len(resposta)):
+        id = resposta[i][0]
+        tarefa = resposta[i][2]
+        data = resposta[i][3]
+        status = resposta[i][6]
+
+        classe = 'categoryRed'
+
+        if status == 'finalizada':
+            classe = 'categoryGreen'
+        elif status == 'iniciada':
+            classe = 'categoryYellow'
+
+        objeto = {'tarefa': tarefa, 'id': id, 'dia': data, 'classe': classe}
+        lista.append(objeto)
+
+    return lista
+
+
+@app.route('/register')
 def registrar_atividade():
     return render_template('register.html')
 
