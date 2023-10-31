bb214bf251a518220269f8762e9649db88c56c7a
Helen de Freitas <82972511+heleafreitas@users.noreply.github.com>
2023-10-31 15:09:56 -0300

    iniciando tarefa

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
-from flask import Flask, render_template, request
+from flask import Flask, redirect, render_template, request, url_for
 import bd_tutorial as db
 
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/home')
 def home():
     lista = listarTarefas()
     return render_template('home.html', data=lista)
 
-@app.route('/iniciarTarefa', methods=['POST'])
-def iniciarTarefa():
-    id = request.form['id']
-    db.execute_query(f"UPDATE activities SET status='iniciada' WHERE id = {id}")
-    return render_template('home.html', data=id)
+@app.route('/iniciar_tarefa/<string:id>', methods=['POST','GET'])
+def iniciar_tarefa(id):
+    identificador = int(id)
+    db.execute_query(f"UPDATE activities SET status='iniciada' WHERE id = {identificador}")
+    return redirect(url_for('home'))
+
+@app.route('/irParaAdicionar', methods=['POST'])
+def irParaAdicionar():
+    print('CLICOU')
+    return redirect(url_for('registrar_atividade'))
 
 def listarTarefas():
     resposta = db.select_from_table(table_name="activities")
@@ -63,7 +69,7 @@ def acao():
 
     db.execute_query(query=db.QUERY_INSERT_TABLE_ACTIVITIES, data=values)
     
-    return render_template('register.html')
+    return redirect(url_for('home'))
 
 
 app.run(debug=True)
